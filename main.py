b307d18ecbff60dae2ae07e97390183fc8d17850
OlesyaSub <lesya.sub@mail.ru>
2021-09-15 23:59:37 +0300

    Add basic implementation

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from model.tracker_model import Expense
+
+app = FastAPI()
+
+
+@app.get("/expenses_tracker/{expense_id}")
+async def get_expense_id(expense_id):
+    return {"expense_id": expense_id}
+
+
+@app.post("/expenses/")
+async def create_item(expense: Expense):
+    return expense
---
+++ model/tracker_model.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class Expense(BaseModel):
+    name: str
+    expenditure: int
+    date: datetime
+    category: str
+    description: Optional[str] = None
