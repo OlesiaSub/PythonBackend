ef2e68f14c91587a58af55ff4ac8b8c1c2e248bd
OlesiaSub <lesya.sub@mail.ru>
2021-12-15 13:23:43 +0500

    Fix structure

--- endpoints/endpoints.py
+++ endpoints/endpoints.py
@@ -1,16 +1,15 @@
-from datetime import timedelta
 from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from starlette import status
 
 from database import db_queries
 from database.database import get_db
 from model import tracker_model
 from model.tracker_model import Expense, User, Group, Token
-from use_case.auth_logic import authenticate_user, ACCESS_TOKEN_EXPIRE_MINUTES, generate_access_token, get_current_user
+from use_case.auth_logic import get_current_user, \
+    process_get_access_token
 from use_case.endpoints_logic import process_create_expense, process_get_group, \
     process_get_user_expense_by_id, process_create_group, process_register_user, process_get_group_expenses
 
@@ -73,17 +72,7 @@ async def get_groups_created_by_user(user_id, db: Session = Depends(get_db)):
 
 @router.post("/token", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = authenticate_user(form_data.username, form_data.password, db)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = generate_access_token(
-        data={"sub": user.name}, expires_delta=access_token_expires
-    )
+    access_token = process_get_access_token(form_data, db)
     return {"access_token": access_token, "token_type": "bearer"}
 
 
--- use_case/auth_logic.py
+++ use_case/auth_logic.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 from typing import Optional
 
 from fastapi import Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import jwt, JWTError
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
@@ -69,3 +69,18 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
                 gender=user.gender,
                 status=user.status,
                 hashed_password=user.hashed_password)
+
+
+def process_get_access_token(form_data: OAuth2PasswordRequestForm, db: Session):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = generate_access_token(
+        data={"sub": user.name}, expires_delta=access_token_expires
+    )
+    return access_token
